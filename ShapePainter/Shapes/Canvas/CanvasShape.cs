8b14eaba777b03274654f5150777abee0c439af9
ClawmanCat <hhy.yhh@live.nl>
2020-03-03 15:48:34 +0100

    Add move function and record history in command

--- ShapePainter/Shapes/Canvas/CanvasShape.cs
+++ ShapePainter/Shapes/Canvas/CanvasShape.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace ShapePainter.Shapes {
     public class CanvasShape : CanvasObject {
         public Shape shape { get; set; }
 
-        public CanvasShape(Shape shape, CanvasObject parent, Point position) : base(position, parent) {
+        public Vector size {
+            get { return new Vector(shape.Width, shape.Height); }
+            set {
+                shape.Width  = value.X;
+                shape.Height = value.Y;
+            }
+        }
+        
+        public bool selected {
+            get { return shape.Stroke == Brushes.Red; }
+            set { shape.Stroke = (value) ? Brushes.Red : Brushes.Black; }
+        }
+
+        public CanvasShape(Shape shape, CanvasObject parent, Vector position) : base(position, parent) {
             this.shape = shape;
         }
 
